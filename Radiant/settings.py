1e6188e0d6edaa63ef1f7deb1eb3ab888a7461bc
devcarkson <decarkson@gmail.com>
2025-10-14 10:24:06 +0100

    note

--- Radiant/settings.py
+++ Radiant/settings.py
@@ -258,3 +258,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- core/management/commands/runserver.py
+++ core/management/commands/runserver.py
@@ -1,27 +1,41 @@
-import threading
-import time
 from django.core.management.commands.runserver import Command as BaseRunserverCommand
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 from django.core.management import call_command
-from django.utils import timezone
+from django.conf import settings
+import os
 
 
 class Command(BaseRunserverCommand):
     def handle(self, *args, **options):
-        # Start the periodic earnings task in a separate thread
-        def run_daily_earnings():
-            while True:
-                # Run the command
-                try:
-                    call_command('add_daily_earnings')
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f'Error running add_daily_earnings: {e}'))
+        # Initialize the scheduler
+        scheduler = BackgroundScheduler()
 
-                # Sleep for 24 hours (86400 seconds)
-                time.sleep(86400)
+        # Add the daily earnings job to run at midnight UTC every day
+        scheduler.add_job(
+            self.run_daily_earnings,
+            trigger=CronTrigger(hour=0, minute=0),  # Run at 00:00 UTC every day
+            id='daily_earnings',
+            name='Add Daily Earnings',
+            replace_existing=True,
+        )
 
-        # Start the thread
-        earnings_thread = threading.Thread(target=run_daily_earnings, daemon=True)
-        earnings_thread.start()
+        # Start the scheduler
+        scheduler.start()
+        self.stdout.write(self.style.SUCCESS('Daily earnings scheduler started'))
 
-        # Run the original runserver
-        super().handle(*args, **options)
+        try:
+            # Run the original runserver
+            super().handle(*args, **options)
+        except KeyboardInterrupt:
+            # Shutdown the scheduler on exit
+            scheduler.shutdown()
+            self.stdout.write(self.style.SUCCESS('Daily earnings scheduler stopped'))
+
+    def run_daily_earnings(self):
+        """Run the add_daily_earnings management command"""
+        try:
+            call_command('add_daily_earnings')
+            self.stdout.write(self.style.SUCCESS('Daily earnings added successfully'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error running add_daily_earnings: {e}'))
