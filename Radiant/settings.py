7537836ad84a3a62732cc7719c1593c5633b8909
devcarkson <decarkson@gmail.com>
2025-10-14 12:25:09 +0100

    ok

--- Radiant/settings.py
+++ Radiant/settings.py
@@ -70,13 +70,6 @@ DEFAULT_FROM_EMAIL='Global Regional Strategy <support@globalregionalstrategy.com
 # EMAIL_HOST_PASSWORD='CeroW4xDByI%'
 # DEFAULT_FROM_EMAIL='Global Regional Strategy <info@kennylass.com.ng>'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'hilda99baker@gmail.com'
-# EMAIL_HOST_PASSWORD = 'xfzu asfz krum gygv'
-
 
 
 JAZZMIN_SETTINGS = {
